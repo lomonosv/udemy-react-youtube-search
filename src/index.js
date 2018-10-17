d5c865d6859460acba0c7cadf36808ea4a76b346
Vladimir Lomonos <lomonosv.developer@gmail.com>
2018-10-17 13:13:33 +0300

    App statefull component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import GOOGLE_API_KEY from './api/google-api-key';
+import { GOOGLE_API_KEY } from './api/google-api-key';
 import SearchBar from './components/SearchBar';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-};
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({
+			key: GOOGLE_API_KEY,
+			term: 'surfboards'
+		}, videos => this.setState({ videos }));
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
